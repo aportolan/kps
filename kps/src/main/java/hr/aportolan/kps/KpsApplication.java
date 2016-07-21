e3c1d15bc55e70085b55fba3e816a4fef0e366c2
pron <antonioportolan@gmail.com>
2016-07-21 16:05:07 +0200

    Inicijalni commit

---
+++ kps/src/main/java/hr/aportolan/kps/KpsApplication.java
@@ -0,0 +1,12 @@
+package hr.aportolan.kps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpsApplication.class, args);
+	}
+}
